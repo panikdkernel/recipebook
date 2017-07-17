6fce57a27bac198c28a6abab5be4a57ef55a9549
Panikd Kernel <panikdkernel@gmail.com>
2017-07-17 21:43:26 +0530

    Update components header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component,OnInit} from '@angular/core';
 
 @Component({
     selector : 'app-header',
@@ -6,6 +6,12 @@ import {Component} from '@angular/core';
     styleUrls : ['./header.component.css']
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+    constructor() {
 
+    }
+
+    ngOnInit() {
+  }
+    
 }
