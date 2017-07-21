a1326365a356eae8e0e7942f5098954bfd670770
Panikd Kernel <panikdkernel@gmail.com>
2017-07-21 16:06:18 +0530

    Update shopping components

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Ingredient} from '../shared/ingredients.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [new Ingredient('Aplles',3),new Ingredient('Oranges',5)];
   constructor() { }
 
   ngOnInit() {
